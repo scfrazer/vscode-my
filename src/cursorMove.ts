96fcc07b5fa4d307f407fbfe7df4124aef5bc812
Scott Frazer <frazer.scott@gmail.com>
2022-01-04 10:15:38 -0500

    Select to char works except for knowing about comments

--- src/cursorMove.ts
+++ src/cursorMove.ts
@@ -38,7 +38,6 @@ export class CursorMove {
     }
 
     public static async selectToChar() {
-
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
@@ -50,20 +49,68 @@ export class CursorMove {
         sbItem.backgroundColor = new vscode.ThemeColor('statusBarItem.errorBackground');
         sbItem.show();
 
-        let typeCommand = vscode.commands.registerCommand('type', arg => {
-            if (arg.text === '\n') {
+        let typeCommand = vscode.commands.registerCommand('type', args => {
+            if (args.text === '\n') {
                 sbItem.dispose();
                 typeCommand.dispose();
                 return;
             }
+
             const document = editor.document;
             editor.selections = editor.selections.map((selection) => {
-                // TODO Fill this in
+                const language = new Language(document.languageId);
+
+                const lastLineNum = document.lineCount - 1;
+                let lineNum = selection.active.line;
+                let colNum = selection.active.character;
+
+                let jumpOverPair = false;
+                let found = false;
+                while (!found) {
+                    const lineText = document.lineAt(lineNum).text;
+                    const lineLen = lineText.length;
+                    // TODO Handle comments somehow
+                    for (let idx = colNum; idx < lineLen; idx += 1) {
+                        if (lineText.charAt(idx) === args.text) {
+                            found = true;
+                            colNum = idx;
+                            break;
+                        }
+                        else if (language.isOpener(lineText.charCodeAt(idx)) || language.isQuotes(lineText.charCodeAt(idx))) {
+                            jumpOverPair = true;
+                            colNum = idx;
+                            break;
+                        }
+                    }
+                    if (jumpOverPair) {
+                        let newPosition = MatchingPair.matchPositionRight(document, new vscode.Position(lineNum, colNum));
+                        if (newPosition === undefined) {
+                            return selection;
+                        }
+                        jumpOverPair = false;
+                        colNum = newPosition.character + 1;
+                        lineNum = newPosition.line;
+                        continue;
+                    }
+                    if (!found) {
+                        if (lineNum === lastLineNum) {
+                            return selection;
+                        }
+                        colNum = 0;
+                        lineNum += 1;
+                    }
+                }
+
+                if (found) {
+                    return (new vscode.Selection(selection.anchor, new vscode.Position(lineNum, colNum)));
+                }
                 return selection;
             });
+
             sbItem.dispose();
             typeCommand.dispose();
         });
+        vscode.commands.executeCommand<void>("revealLine", { lineNumber: editor.selection.active.line });
     }
 
     public static async previousParagraph(args: any = {}) {
