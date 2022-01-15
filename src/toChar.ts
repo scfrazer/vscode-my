09a65e72f33320ad9a50ebb9e6fce5e346980f20
Scott Frazer <frazer.scott@gmail.com>
2022-01-15 15:57:02 -0500

    Sel/del to char

--- src/toChar.ts
+++ src/toChar.ts
@@ -6,6 +6,7 @@ import { MatchingPair } from './matchingPair';
 export class ToChar {
 
     public static async select() {
+
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
@@ -23,64 +24,102 @@ export class ToChar {
                 typeCommand.dispose();
                 return;
             }
-
             const document = editor.document;
             editor.selections = editor.selections.map((selection) => {
-                const language = new Language(document.languageId);
-
-                const lastLineNum = document.lineCount - 1;
-                let lineNum = selection.active.line;
-                let colNum = selection.active.character;
-
-                let jumpOverPair = false;
-                let found = false;
-                while (!found) {
-                    const lineText = document.lineAt(lineNum).text;
-                    const lineLen = lineText.length;
-                    for (let idx = colNum; idx < lineLen; idx += 1) {
-                        if (lineText.charAt(idx) === args.text) {
-                            found = true;
-                            colNum = idx;
-                            break;
-                        }
-                        else if (language.isOpener(lineText.charCodeAt(idx)) || language.isQuotes(lineText.charCodeAt(idx))) {
-                            jumpOverPair = true;
-                            colNum = idx;
-                            break;
-                        }
-                    }
-                    if (jumpOverPair) {
-                        let newPosition = MatchingPair.matchPositionRight(document, new vscode.Position(lineNum, colNum));
-                        if (newPosition === undefined) {
-                            return selection;
-                        }
-                        jumpOverPair = false;
-                        colNum = newPosition.character + 1;
-                        lineNum = newPosition.line;
-                        continue;
-                    }
-                    if (!found) {
-                        if (lineNum === lastLineNum) {
-                            return selection;
-                        }
-                        colNum = 0;
-                        lineNum += 1;
-                    }
-                }
-
-                if (found) {
-                    return (new vscode.Selection(selection.anchor, new vscode.Position(lineNum, colNum)));
+                const newPosition = ToChar._upTo(document, selection.active, args.text);
+                if (newPosition !== undefined) {
+                    return (new vscode.Selection(selection.anchor, newPosition));
                 }
                 return selection;
             });
-
             sbItem.dispose();
             typeCommand.dispose();
         });
+
         vscode.commands.executeCommand<void>("revealLine", { lineNumber: editor.selection.active.line });
     }
 
     public static async delete() {
-        // FIXME
+
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        const sbItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
+        sbItem.text = "Delete to character ...";
+        sbItem.tooltip = "'Return' will exit without deleting";
+        sbItem.backgroundColor = new vscode.ThemeColor('statusBarItem.errorBackground');
+        sbItem.show();
+
+        let typeCommand = vscode.commands.registerCommand('type', args => {
+            if (args.text === '\n') {
+                sbItem.dispose();
+                typeCommand.dispose();
+                return;
+            }
+            const document = editor.document;
+            editor.edit((editBuilder) => {
+                editor.selections.map((selection) => {
+                    const newPosition = ToChar._upTo(document, selection.active, args.text);
+                    if (newPosition !== undefined) {
+                        const range = new vscode.Range(selection.anchor, newPosition);
+                        editBuilder.delete(range);
+                    }
+                });
+            });
+            sbItem.dispose();
+            typeCommand.dispose();
+        });
+    }
+
+    private static _upTo(document: vscode.TextDocument, startPosition: vscode.Position, text: string): vscode.Position | undefined {
+
+        const language = new Language(document.languageId);
+
+        const lastLineNum = document.lineCount - 1;
+        let lineNum = startPosition.line;
+        let colNum = startPosition.character;
+
+        let jumpOverPair = false;
+        let found = false;
+        while (!found) {
+            const lineText = document.lineAt(lineNum).text;
+            const lineLen = lineText.length;
+            for (let idx = colNum; idx < lineLen; idx += 1) {
+                if (lineText.charAt(idx) === text) {
+                    found = true;
+                    colNum = idx;
+                    break;
+                }
+                else if (language.isOpener(lineText.charCodeAt(idx)) || language.isQuotes(lineText.charCodeAt(idx))) {
+                    jumpOverPair = true;
+                    colNum = idx;
+                    break;
+                }
+            }
+            if (jumpOverPair) {
+                let newPosition = MatchingPair.matchPositionRight(document, new vscode.Position(lineNum, colNum));
+                if (newPosition === undefined) {
+                    return undefined;
+                }
+                jumpOverPair = false;
+                colNum = newPosition.character + 1;
+                lineNum = newPosition.line;
+                continue;
+            }
+            if (!found) {
+                if (lineNum === lastLineNum) {
+                    return undefined;
+                }
+                colNum = 0;
+                lineNum += 1;
+            }
+        }
+
+        if (found) {
+            return new vscode.Position(lineNum, colNum);
+        }
+        return undefined;
     }
 }
