52cfb83acb383806905d5c2e7f7a61160accfc6e
Scott Frazer <frazer.scott@gmail.com>
2024-01-22 07:41:29 -0500

    If in the middle of a word and completion fails, try again with just the prefix part

--- src/completionProvider.ts
+++ src/completionProvider.ts
@@ -9,12 +9,7 @@ export class InlineCompletionItemProvider implements vscode.InlineCompletionItem
     ): vscode.ProviderResult<vscode.InlineCompletionItem[] | vscode.InlineCompletionList> {
 
         const automaticallyTriggered: boolean = (context.triggerKind === vscode.InlineCompletionTriggerKind.Automatic);
-        const results: Array<vscode.InlineCompletionItem> = [];
-        const previousCompletions = new Map<string, boolean>;
-        const numLinesToSearch = 1000;
-
-        let searchRe;
-        let range: vscode.Range;
+        let results: Array<vscode.InlineCompletionItem> | undefined = [];
 
         let prefix = _wordBeforePosition(document, position);
         let suffix = _wordAfterPosition(document, position);
@@ -31,78 +26,101 @@ export class InlineCompletionItemProvider implements vscode.InlineCompletionItem
             return;
         }
 
-        searchRe = new RegExp(`\\b(${prefix}[a-zA-Z0-9_]*)${suffix}\\b`, 'g');
-        const prefixLength = prefix.length;
-        range = new vscode.Range(position.translate(0, -1 * prefixLength), position);
-
-        let lineNum = position.line;
-        let colNum = position.character - 1;
-        if (colNum < 0) {
-            colNum = 0;
-        }
-        let targetLineNum = (numLinesToSearch >= (lineNum + 1)) ? 0 : (lineNum - numLinesToSearch + 1);
-        while (lineNum > targetLineNum) {
-            let text = document.lineAt(lineNum).text;
-            if (colNum > 0) {
-                text = text.substring(0, colNum);
-            }
-            const matches = [...text.matchAll(searchRe)].reverse();
-            for (const match of matches) {
-                if (match?.index === undefined) {
-                    continue;
-                }
-                const candidate = match[1];
-                if (candidate.length === prefixLength) {
-                    if (automaticallyTriggered) {
-                        return;
-                    }
-                    continue;
-                }
-                if (previousCompletions.has(candidate)) {
-                    continue;
-                }
-                previousCompletions.set(candidate, true);
-                const completionItem = new vscode.InlineCompletionItem(candidate, range);
-                results.push(completionItem);
-            }
-            lineNum -= 1;
-            colNum = 0;
-        }
-
-        lineNum = position.line;
-        colNum = position.character + 1;
-        const lastLineNum = document.lineCount;
-        targetLineNum = ((lineNum + numLinesToSearch) >= lastLineNum) ? lastLineNum : (lineNum + numLinesToSearch);
-        while (lineNum < targetLineNum) {
-            let text = document.lineAt(lineNum).text;
-            if (colNum > 0) {
-                text = text.substring(colNum);
-            }
-            const matches = text.matchAll(searchRe);
-            for (const match of matches) {
-                if (match?.index === undefined) {
-                    continue;
-                }
-                const candidate = match[1];
-                if (candidate.length === prefixLength) {
-                    if (automaticallyTriggered) {
-                        return;
-                    }
-                    continue;
-                }
-                if (previousCompletions.has(candidate)) {
-                    continue;
-                }
-                previousCompletions.set(candidate, true);
-                const completionItem = new vscode.InlineCompletionItem(candidate, range);
-                results.push(completionItem);
-            }
-            lineNum += 1;
-            colNum = 0;
+        results = _getCompletionResults(document, position, context, prefix, suffix);
+        if (prefix !== "" && suffix !== "" && results !== undefined && results.length === 0) {
+            results = _getCompletionResults(document, position, context, prefix, "");
         }
 
         return results;
     }
+}
+
+function _getCompletionResults(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    context: vscode.InlineCompletionContext,
+    prefix: string, suffix: string
+): Array<vscode.InlineCompletionItem> | undefined {
+
+    const automaticallyTriggered: boolean = (context.triggerKind === vscode.InlineCompletionTriggerKind.Automatic);
+    const results: Array<vscode.InlineCompletionItem> = [];
+    const previousCompletions = new Map<string, boolean>;
+    const numLinesToSearch = 1000;
+
+    let searchRe;
+    let range: vscode.Range;
+
+    searchRe = new RegExp(`\\b(${prefix}[a-zA-Z0-9_]*)${suffix}\\b`, 'g');
+    const prefixLength = prefix.length;
+    range = new vscode.Range(position.translate(0, -1 * prefixLength), position);
+
+    let lineNum = position.line;
+    let colNum = position.character - 1;
+    if (colNum < 0) {
+        colNum = 0;
+    }
+    let targetLineNum = (numLinesToSearch >= (lineNum + 1)) ? 0 : (lineNum - numLinesToSearch + 1);
+    while (lineNum > targetLineNum) {
+        let text = document.lineAt(lineNum).text;
+        if (colNum > 0) {
+            text = text.substring(0, colNum);
+        }
+        const matches = [...text.matchAll(searchRe)].reverse();
+        for (const match of matches) {
+            if (match?.index === undefined) {
+                continue;
+            }
+            const candidate = match[1];
+            if (candidate.length === prefixLength) {
+                if (automaticallyTriggered) {
+                    return undefined;
+                }
+                continue;
+            }
+            if (previousCompletions.has(candidate)) {
+                continue;
+            }
+            previousCompletions.set(candidate, true);
+            const completionItem = new vscode.InlineCompletionItem(candidate, range);
+            results.push(completionItem);
+        }
+        lineNum -= 1;
+        colNum = 0;
+    }
+
+    lineNum = position.line;
+    colNum = position.character + 1;
+    const lastLineNum = document.lineCount;
+    targetLineNum = ((lineNum + numLinesToSearch) >= lastLineNum) ? lastLineNum : (lineNum + numLinesToSearch);
+    while (lineNum < targetLineNum) {
+        let text = document.lineAt(lineNum).text;
+        if (colNum > 0) {
+            text = text.substring(colNum);
+        }
+        const matches = text.matchAll(searchRe);
+        for (const match of matches) {
+            if (match?.index === undefined) {
+                continue;
+            }
+            const candidate = match[1];
+            if (candidate.length === prefixLength) {
+                if (automaticallyTriggered) {
+                    return undefined;
+                }
+                continue;
+            }
+            if (previousCompletions.has(candidate)) {
+                continue;
+            }
+            previousCompletions.set(candidate, true);
+            const completionItem = new vscode.InlineCompletionItem(candidate, range);
+            results.push(completionItem);
+        }
+        lineNum += 1;
+        colNum = 0;
+    }
+
+    return results;
 }
 
 function _wordBeforePosition(document: vscode.TextDocument, position: vscode.Position): string | undefined {
