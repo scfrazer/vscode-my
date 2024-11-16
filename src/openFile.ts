5a9ad12cf17a4bfc468242b319c18b411bb91e7d
Scott Frazer <frazer.scott@gmail.com>
2024-11-16 08:35:28 -0500

    Update openFile functionality to improve filename extraction and environment variable expansion

--- src/openFile.ts
+++ src/openFile.ts
@@ -11,61 +11,62 @@ export class OpenFile {
         const position = editor.selection.active;
         const lineText = document.lineAt(position.line).text;
 
-        const beforeRegex = /([-a-zA-Z0-9_./]+)$/;
-        const afterRegex = /^([-a-zA-Z0-9_./]+)(?:[:,@;{([]\s*(\d+))?/;
-        let filename = "";
-        let lineNumber = 0;
+        let candidate = "";
 
         const beforeText = lineText.slice(0, position.character);
+        const beforeRegex = /(?<envVar>~|\$([A-Z_]+\b)|\$\{([A-Z_]+)\}|\$\(([A-Z_]+)\))?(?<path>[-a-zA-Z0-9_./\\]+)$/;
         const beforeMatch = beforeRegex.exec(beforeText);
         if (beforeMatch) {
-            filename = beforeMatch[1];
+            candidate = beforeMatch[0];
         }
 
-        const afterText = lineText.slice(position.character);
-        const afterMatch = afterRegex.exec(afterText);
-        if (afterMatch) {
-            filename += afterMatch[1];
-            if (afterMatch[2]) {
-                lineNumber = parseInt(afterMatch[2], 10) - 1;
-            }
-        }
-
-        if (filename) {
-            // Replace tilde with $HOME
-            filename = filename.replace(/~/g, process.env.HOME || "");
-
-            // Expand environment variables
-            filename = filename.replace(
-                /\$([A-Z_]+\b)|\$\{([A-Z_]+)\}|\$\(([A-Z_]+)\)/g,
-                (_, plainEnvVar, curlyEnvVar, roundEnvVar) => {
-                    return process.env[plainEnvVar || curlyEnvVar || roundEnvVar] || "";
-                }
-            );
-
-            // Normalize the path to use forward slashes and replace multiple slashes with a single slash
-            const normalizedPath = filename.replace(/\\/g, "/").replace(/\/+/g, "/");
-            let fullPath: vscode.Uri;
-
-            if (vscode.env.remoteName) {
-                const remoteAuthority = vscode.env.remoteName;
-                fullPath = vscode.Uri.parse(`vscode-remote://${remoteAuthority}${normalizedPath}`);
-            } else {
-                fullPath = vscode.Uri.file(normalizedPath);
-            }
-
-            try {
-                await vscode.workspace.fs.stat(fullPath);
-                await OpenFile.openUriAtLine(fullPath, lineNumber);
-            } catch (error) {
-                vscode.window.showErrorMessage(`Error opening file: ${error}`);
-            }
-        } else {
+        candidate += lineText.slice(position.character);
+        const fullRegex =
+            /^(?<envVar>~|\$([A-Z_]+\b)|\$\{([A-Z_]+)\}|\$\(([A-Z_]+)\))?(?<path>[-a-zA-Z0-9_./\\]+)(?:\s*[:,@;{([]\s*(?<line>\d+))?/;
+        const fullMatch = fullRegex.exec(candidate);
+        if (!fullMatch) {
             vscode.window.showErrorMessage("No filename found under cursor.");
+            return;
+        }
+
+        let filename = (fullMatch.groups?.envVar || "") + fullMatch.groups?.path;
+
+        let lineNumber = 0;
+        if (fullMatch.groups?.line) {
+            lineNumber = parseInt(fullMatch.groups.line, 10) - 1;
+        }
+
+        // Replace tilde with $HOME
+        filename = filename.replace(/~/g, process.env.HOME || process.env.HOMEPATH || "");
+
+        // Expand environment variables
+        filename = filename.replace(
+            /\$([A-Z_]+\b)|\$\{([A-Z_]+)\}|\$\(([A-Z_]+)\)/g,
+            (_, plainEnvVar, curlyEnvVar, roundEnvVar) => {
+                return process.env[plainEnvVar || curlyEnvVar || roundEnvVar] || "";
+            }
+        );
+
+        // Normalize the path to use forward slashes and replace multiple slashes with a single slash
+        const normalizedPath = filename.replace(/\\/g, "/").replace(/\/+/g, "/");
+
+        let fullPath: vscode.Uri;
+        if (vscode.env.remoteName) {
+            const remoteAuthority = vscode.env.remoteName;
+            fullPath = vscode.Uri.parse(`vscode-remote://${remoteAuthority}${normalizedPath}`);
+        } else {
+            fullPath = vscode.Uri.file(normalizedPath);
+        }
+
+        try {
+            await vscode.workspace.fs.stat(fullPath);
+            await OpenFile.openUriAtLine(fullPath, lineNumber);
+        } catch (error) {
+            vscode.window.showErrorMessage(`Error opening file: ${error}`);
         }
     }
 
-    public static async openUriAtLine(uri: vscode.Uri, line: number) {
+    public static async openUriAtLine(uri: vscode.Uri, line: number = 0) {
         const document = await vscode.workspace.openTextDocument(uri);
         const editor = await vscode.window.showTextDocument(document);
         const position = new vscode.Position(line, 0);
